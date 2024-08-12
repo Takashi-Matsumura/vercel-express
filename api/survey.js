401e239b7be5d579fba6cd1c4aa5e6f563eecae0
Takashi-Matsumura <mats@coral.plala.or.jp>
2024-08-12 20:11:54 +0900

    chore: Update package.json and survey.js

---
+++ api/survey.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const https = require('https');
+const app = express();
+
+
+const server = https.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+const PORT = 5000;
+const surveys = {
+    survey0: {
+        id: 0,
+        question: "出席を確認します!",
+        description: "[出席]ボタンをタップしてください。",
+        options: ["出席"]
+    },
+    survey1: {
+        id: 1,
+        question: "あなたの知っている有名なIT企業は?",
+        description: "複数回答可（一社ずつ回答）",
+        options: []
+    },
+    survey2: {
+        id: 2,
+        question: "どのエンジニアに興味がありますか?",
+        description: "",
+        options: ["ソフト", "システム", "ネットワーク", "クラウド", "その他"]
+    }
+};
+let surveyResults = [0, 0, 0, 0, 0];
+const resetSurveyResults = () => {
+    surveyResults = [0, 0, 0, 0, 0];
+};
+
+io.on('connection', (socket) => {
+    console.log('connected to client');
+
+    socket.on('disconnect', () => {
+        console.log('disconnected from client');
+    });
+
+    socket.on('select_survey', (survey) => {
+        resetSurveyResults();
+        io.emit('survey_changed', surveys[survey]);
+    });
+
+    socket.on('send_message', (data) => {
+        console.log(data);
+        io.emit('receive_message', data);
+    });
+
+    socket.on('send_vote', (index) => {
+        if (index >= 0 && index < surveyResults.length) {
+            surveyResults[index]++;
+            console.log(surveyResults);
+        } else {
+            console.error('Invalid index');
+        }
+        io.emit('receive_votes', surveyResults);
+    });
+
+    
+});
+
+server.listen(PORT, () => {
+    console.log(`HTTPS server is running on port ${PORT}`);
+});
